9043275f6c6d7476dd3bf257e74f1037b54a3dbf
Grace Chen <gchen19@gmail.com>
2021-04-25 22:40:33 -0500

    new fetch code, api post works

--- src/api/apipost.js
+++ src/api/apipost.js
@@ -1,13 +1,19 @@
-async function run() {
-	const formData = new FormData();
+function run() {
+	var formdata = new FormData();
 	const fileField = document.querySelector('input[type="file"]');
-	formData.append('test', fileField.files[0]);
+	formdata.append("file", fileField.files[0]);
+	formdata.append("Metadata", "{\"file_name\": \"file.json\"}");
 
-	const response = await fetch('https://efaeig3s60.execute-api.us-west-2.amazonaws.com/v2/upload?Content-Type=multipart/form-data', {
-		method: 'POST',
-		body: formData
-	});
+	var requestOptions = {
+	  mode: 'no-cors',
+	  method: 'POST',
+	  body: formdata,
+	  redirect: 'follow'
+	};
 
-	const content = await response.json();
-	console.log(content);
+	fetch("https://efaeig3s60.execute-api.us-west-2.amazonaws.com/v2/upload?Content-Type=multipart/form-data", requestOptions)
+	  .then(response => response.text())
+	  .then(result => console.log(result))
+	  .catch(error => console.log('error', error));
+	console.log("here");
 }
